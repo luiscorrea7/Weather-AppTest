2af9826ecca2f5f8355c3067e39c26996f7f671d
luis correa <ylss512c@gmail.com>
2023-10-11 22:03:36 -0300

    first testing

--- Js/app.js
+++ Js/app.js
@@ -1,1 +1,11 @@
-console.log('hello')
+const getGeoData = async () => {
+  try {
+    const firstData = await fetch('http://api.openweathermap.org/geo/1.0/direct?q=buenosaires&limit=5&appid=c254dde7ee55ef9170dcb20399e894d1')
+    const finalData = await firstData.json()
+    console.log(finalData[0].lat)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+getGeoData();
